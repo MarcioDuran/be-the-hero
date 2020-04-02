4ad73ef8c7648d63d217a5d4fc375e2eadf5ecd1
Marcio Duran <marcioduran@gmail.com>
2020-04-02 13:37:57 -0300

    Aplicação Back-Front RocketSeat

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,24 +1,21 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
-const OngController =  require('./controllers/OngController');
-const IncidentsController= require('./controllers/IncidentController');
-const ProfileController= require('./controllers/ProfileController');
-const SessionController= require('./controllers/SessionController'); 
+const routes = express.Router();
 
-const routes= express.Router();
-
-routes.post('/sessions')
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
 routes.get('/profile', ProfileController.index);
 
-routes.get('/incidents', IncidentsController.index);
-routes.post('/incidents', IncidentsController.create);
-routes.delete('/incidents/:id',IncidentsController.delete);
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
-routes.post('/incidents')
-
-module.exports=routes;
+module.exports = routes;
